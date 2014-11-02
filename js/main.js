a972e8a5b05588d69fdb1817fe8b9993851866f5
Anselm Hannemann <info@anselm-hannemann.com>
2014-11-02 22:07:52 +0100

    Add JS for navigation, remove old stuff.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-/* Author:
+/**
+ * Main.js
+ */
 
-*/
+(function (global) {
+	'use strict';
+
+	// Check if we are dealing with mobile
+	var isMobile = function () {
+		return (window.innerWidth < 481);
+	};
+
+	window.addEventListener('DOMContentLoaded', function () {
+		var navigation = document.querySelector('.header__nav');
+
+		if (isMobile) {
+			navigation.classList.toggle('is-narrow');
+
+			navigation.addEventListener('click', function () {
+				navigation.classList.toggle('is-expanded');
+			});
+		}
+	});
+
+}(window));
